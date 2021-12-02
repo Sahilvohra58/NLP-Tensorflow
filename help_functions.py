d856653e35b36f0fdb5eee28afac8f77d5f6064f
vohrasahil58 <vohrasahil58@gmail.com>
2021-12-02 11:20:26 -0500

    Some handy helpful functions

---
+++ help_functions.py
@@ -0,0 +1,99 @@
+import zipfile
+import urllib.request
+import os
+import shutil
+
+def download_and_unzip(url, file_name):
+    urllib.request.urlretrieve(url, file_name)
+    print('Beginning file download...')
+
+    #File Extraction
+    zip_ref =  zipfile.ZipFile(file_name, 'r')
+    zip_ref.extractall()
+    zip_ref.close()
+    if os.path.exists('__MACOSX'):
+        shutil.rmtree('__MACOSX')
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+    return
+
+
+import os 
+def dir_explore(dir_path):
+    for root, dirs, files in os.walk(dir_path):
+        # root : Prints out directories only from what you specified.
+        # dirs : Prints out sub-directories from root.
+        # files : Prints out all files from root and directories.
+        print(f"There are {len(dirs)} directories and {len(files)} files in {root} folder.")
+    return
+
+
+import datetime
+import tensorflow as tf
+def create_callbacks(tensorboard_dir_name, checkpoint_path, experiment_name, monitor='val_accuracy', patience= 5, verbose= 0, save_weights_only=False):
+    log_dir = tensorboard_dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor=monitor, 
+                                                                patience=patience, 
+                                                                verbose=verbose)
+    check_point_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                                save_weights_only=save_weights_only,
+                                                                verbose=verbose)
+    print(f"Saving TensorBoard log files to: {log_dir}")
+    return [tensorboard_callback, early_stopping_callback, check_point_callback]
+
+
+
+import matplotlib.pyplot as plt
+def plot_loss_curves(model_history):
+
+    print(model_history.history.keys())
+    # summarize history for accuracy
+    plt.plot(model_history.history['accuracy'])
+    plt.plot(model_history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.show()
+
+    plt.plot(model_history.history['loss'])
+    plt.plot(model_history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend()
+    plt.show()
+
+
+import matplotlib.image as mpimg
+import random
+def view_random_image(train_data_path):
+    target_class = random.choice(os.listdir(train_data_path)) # choose a random class
+    target_dir = train_data_path + '/' + target_class # create the target directory
+    random_image = random.choice(os.listdir(target_dir)) # choose a random image from target directory
+    random_image_path = target_dir + "/" + random_image # create the choosen random image path
+    img = mpimg.imread(random_image_path) # read in the chosen target image
+    plt.imshow(img) # plot the target image
+    plt.title(f"Original random image from class: {target_class}")
+    plt.axis(False); # turn off the axes
+    return img, target_class
+
+
+from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, recall_score, f1_score
+
+def matrices_calc(y_true, y_pred):
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    (tn, fp, fn, tp) = confusion_matrix(y_true, y_pred).ravel()
+    #print(f'accuracy: {accuracy:.4f}\nPrecision: {precision:.4f}\nRecall: {recall}\nF1-Score: {f1:.4f}\n(tn, fp, fn, tp): {(tn, fp, fn, tp)}')
+    metrics = {'accuracy': accuracy,
+                'precision': precision,
+                'recall': recall,
+                'F1-Score': f1,
+                '(tn, fp, fn, tp)': (tn, fp, fn, tp)}
+    return metrics
+
